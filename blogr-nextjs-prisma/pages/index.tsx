347472155ee5f9a6dbef14f77b902b5193045e56
Vayneil <mkrakowski.mail@gmail.com>
2022-05-30 06:12:55 +0200

    search field

--- blogr-nextjs-prisma/pages/index.tsx
+++ blogr-nextjs-prisma/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { GetStaticProps } from "next"
 import Layout from "../components/Layout"
 import Post, { PostProps } from "../components/Post"
@@ -17,22 +17,60 @@ export const getStaticProps: GetStaticProps = async () => {
   return { props: { feed } };
 }
 
+const filterFeed = (searchValue, posts, setResults) => {
+  let results = [];
+  if (searchValue === "") return setResults(posts);
+  for (const post of posts) {
+    let pattern = new RegExp(searchValue, "i");
+    if (0
+      // [...searchValue].includes(post.title) ||
+      // searchValue === post.title ||
+      // searchValue.split(" ").includes(post.title)
+      // (pattern.match() || )
+    ) {
+      results.push(post);
+    }
+  }
+  setResults(results);
+}
+
 type Props = {
   feed: PostProps[]
 }
 
 const Blog: React.FC<Props> = (props) => {
+  const [filterByName, setFilterByName] = useState("");
+  const [filterByNameResults, setFilterByNameResults] = useState([]);
+
   return (
     <Layout>
       <div className="page">
         <h1>Public Feed</h1>
+        <input 
+          type="text"
+          onChange={(e) => setFilterByName(e.target.value)}
+          onKeyUp={(e) =>
+            filterFeed(
+              (e.target as HTMLInputElement).value,
+              props.feed,
+              setFilterByNameResults
+            )
+          }></input>
         <main>
-          {props.feed.map((post) => (
+          {/* {props.feed.map((post) => (
             <div key={post.id} className="post">
               <Post post={post} />
             </div>
-          ))}
+          ))} */}
+          {filterByNameResults && filterByNameResults.length
+            ? filterByNameResults.map((post) => (
+              <div key={post.id} className="post">
+                <Post post={post} />
+              </div>
+              ))
+            : ""}
         </main>
+        
       </div>
       <style jsx>{`
         .post {
